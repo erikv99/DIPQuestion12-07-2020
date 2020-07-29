648319f5b316fd9ca2a86ba6df96a8fdbb902f53
Erik Veenstra <zerikveenstraz@gmail.com>
2020-07-29 14:48:57 +0200

    Noticed i forgot to implement the "return -1 if target not found" feature. Now has been added

--- DailyProblem12_07_2020.py
+++ DailyProblem12_07_2020.py
@@ -7,13 +7,18 @@ class Solution:
       # Adding all the indexes of elements of the array that are equal to our target to a new array
       indices = [i for i, currentNum in enumerate(arr) if currentNum == target]
       
+      # If there is 1 or 0 indices found (num is only present once, or not at all)
+      if (len(indices) < 2):
+
+          return [-1]
+
       # Getting the first and last index and returning the result
       result = [indices[0], indices[len(indices) - 1]]
       return result
 
 # Test program 
 arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
-target = 2
+target = 9
 
 sol = Solution().getRange(arr, target)
 print(sol)
